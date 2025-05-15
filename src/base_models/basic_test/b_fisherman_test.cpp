41fdfdae5f23b3178d5101658f9877a46cad1b0e
Erik Ramos <erikram312@gmail.com>
2025-05-15 09:41:52 -0700

    test cases update

--- src/base_models/basic_test/b_fisherman_test.cpp
+++ src/base_models/basic_test/b_fisherman_test.cpp
@@ -8,12 +8,12 @@
 int main(int argc, char *argv[]){
 	std::cout << "Tester running.....\n";
 	Fisherman test_fisherman;
-    test_fisherman.name="Test Guy";
+    test_fisherman._name="Test Guy";
 	assert(typeid(test_fisherman)==typeid(Fisherman));
-	assert(test_fisherman.name != " ");
-    assert(test_fisherman.catch_bag.empty() == true);
+	assert(test_fisherman._name != " ");
+    assert(test_fisherman._catchBag.empty() == true);
     std::cout << "\n";
-    std::cout << "Test fisherman name: " << test_fisherman.name << "\n";
+    std::cout << "Test fisherman name: " << test_fisherman._name << "\n";
     std::cout << "\n";
 	std::cout << "End test [PASS]\n";
 
